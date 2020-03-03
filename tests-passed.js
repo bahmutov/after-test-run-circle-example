32b0ca91bb729efbc7af1946a28cd3c043e325eb
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-03-03 16:48:00 -0500

    use globby to find all files

--- tests-passed.js
+++ tests-passed.js
@@ -1,11 +1,19 @@
 const fs = require('fs')
 const path = require('path')
-const filename = path.join(__dirname, 'test-results.json')
+const globby = require('globby')
 
-console.log('checking file %s to see if tests succeeded', filename)
-// any error thrown means the tests failed
-const results = JSON.parse(fs.readFileSync(filename, 'utf8'))
-if (results.totalFailed !== 0) {
-  throw new Error(`Test results have total failed !== 0, but ${results.totalFailed}`)
-}
-console.log('tests passed')
+globby('test-results-*.json').then(files => {
+  console.log('found files')
+  console.log(files.join('\n'))
+
+  files.forEach((filename => {
+    console.log('checking file %s to see if tests succeeded', filename)
+    // any error thrown means the tests failed
+    const results = JSON.parse(fs.readFileSync(filename, 'utf8'))
+    if (results.totalFailed !== 0) {
+      throw new Error(`Test results have total failed !== 0, but ${results.totalFailed}`)
+    }
+    console.log('tests passed')
+  }))
+})
+
